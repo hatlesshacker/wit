b3b6a5cdfb4cf9a4f469153d98eac227e0902f4b
Syed Nasim <nasim@warmachine.rf.gd>
2020-04-05 16:15:44 +0530

    Changes to be committed: 	modified:   Makefile.am 	modified:   Makefile.in 	modified:   client.cpp 	new file:   includes/cli11.h

--- wit-client/client.cpp
+++ wit-client/client.cpp
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include <iostream>
+#include <cli11.h>
+using namespace std;
 
 int main(int argc, char* argv[]) {
-    printf("wit client\n");
-    return 0;
+
+    std::string topline("wit client - version ");
+    std::string add(PACKAGE_VERSION);
+    topline.append(add);
+
+    cout << topline << endl;
+    CLI::App app{topline};
+
+    string init_loc = "."; //Default
+    app.add_option("-i,--init", init_loc, "Initialize a wit repository");
+
+    CLI11_PARSE(app, argc, argv);     
+    return 0; 
 }
