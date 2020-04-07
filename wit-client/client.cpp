a89758a0a930a2c36f0c4776f1f70eb09a0cc7e5
Syed Nasim <nasim@warmachine.rf.gd>
2020-04-07 15:08:01 +0530

    Changes to be committed: 	modified:   client.cpp

--- wit-client/client.cpp
+++ wit-client/client.cpp
@@ -11,9 +11,29 @@ int main(int argc, char* argv[]) {
     CLI::App app{topline};
 
     app.require_subcommand(1);
-    
-    string init_loc = "."; //Default
-    app.add_option("-i,--init", init_loc, "Initialize a wit repository");
+
+    //INITIALIZE REPO
+    auto init = app.add_subcommand("init", "Initialize a wit project");
+    std::vector<std::string> remote_loc;
+    init->add_option("-r, --remote", remote_loc, "Initialize from remote location");
+
+    init->callback([&]() {
+    if(!remote_loc.empty()) {
+            std::cout << "Initializing wit repo from ";
+            for (auto i = remote_loc.begin(); i != remote_loc.end(); ++i){
+                std::cout << *i << ' ';
+            }
+            std::cout << "..\n";
+        } else {
+            std::cout << "Initializing local wit repo..\n";
+        }
+    });
+
+    //SHOW STATUS
+    auto status = app.add_subcommand("status", "Print changes since last commit");
+    status->callback([&]() {
+        cout << "STATUS\n"; //TODO
+    });
 
     CLI11_PARSE(app, argc, argv);     
     return 0; 
